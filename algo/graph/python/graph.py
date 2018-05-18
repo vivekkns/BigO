458a074947f87cd4b79031c0d82a68ce8ff0b36b
Vivek Kumar <vivekkumar1987@gmail.com>
2018-05-18 08:42:41 +0530

    added DFS

--- algo/graph/python/graph.py
+++ algo/graph/python/graph.py
@@ -1,4 +1,4 @@
-
+from collections import defaultdict
 
 #  Graph representation
 
@@ -8,4 +8,39 @@
 # 2. Adjacency list
 # [ [], [], [], [] ... []]
 #
+
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def DFS(self, v, visited=None):
+        if visited is None:
+            visited = dict()
+            for i in self.graph:
+                visited[i] = False
+        print('visited=', v)
+        visited[v] = True
+        for e in self.graph[v]:
+            if not visited[e]:
+                self.DFS(e, visited)
+
+if __name__ == '__main__':
+    g = Graph()
+    g.addEdge('A', 'B')
+    g.addEdge('B', 'H')
+    g.addEdge('B', 'C')
+    g.addEdge('C', 'E')
+    g.addEdge('C', 'D')
+    g.addEdge('E', 'F')
+    g.addEdge('E', 'G')
+    g.addEdge('E', 'H')
+
+    print(g.graph)
+
+    g.DFS('A')
 
