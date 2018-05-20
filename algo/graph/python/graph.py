1d75f12cc7ee2f1d588c957fb2a4b950cc579c2a
Vivek Kumar <vivekkumar1987@gmail.com>
2018-05-20 16:43:13 +0530

    added BFS

--- algo/graph/python/graph.py
+++ algo/graph/python/graph.py
@@ -26,18 +26,34 @@ class Graph:
 
     def addEdge(self, u, v):
         self.graph[u].append(v)
+        _ = self.graph[v]
 
-    def DFS(self, v, visited=None):
-        if visited is None:
-            visited = [False] * len(self.graph)
-            
+    def DFS_util(self, v, visited):
         print('visited=', v)
         visited[v] = True
-        for e in self.graph[v]:
-            if not visited[e]:
-                self.DFS(e, visited)
+        for u in self.graph[v]:
+            if not visited[u]:
+                self.DFS_util(u, visited)
+
+    def DFS(self, v):
+        visited = [False] * len(self.graph)
+        self.DFS_util(v, visited)
+
+    def BFS(self, v):
+        visited = [False] * len(self.graph)
+        q = list()
+        q.append(v)
+        while len(q) > 0:
+            t = q.pop()
+            if not visited[t]:
+                print('visited=', t)
+                visited[t] = True
+            for u in self.graph[t]:
+                if not visited[u]:
+                    q.append(u)
 
 if __name__ == '__main__':
+
     g = Graph()
     g.addUEdge(0, 1)
     g.addUEdge(1, 7)
@@ -47,8 +63,10 @@ if __name__ == '__main__':
     g.addUEdge(4, 5)
     g.addUEdge(4, 6)
     g.addUEdge(4, 7)
-
     print(g.graph)
 
+    print('----- DFS -----')
     g.DFS(0)
 
+    print('----- BFS -----')
+    g.BFS(0)
