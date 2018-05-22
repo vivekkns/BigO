c8558efbbcbfe96e3e4c1512b3ef480f8d794d2a
Vivek Kumar <vivekkumar1987@gmail.com>
2018-05-22 07:43:27 +0530

    bellman-ford algo

--- algo/graph/python/shortest_path.py
+++ algo/graph/python/shortest_path.py
@@ -82,17 +82,17 @@ class PriorityMapQueue:
 
 class Graph:
     def __init__(self):
-        self.graph = defaultdict(list)
+        self.adj = defaultdict(list)
 
     def addedge(self, u, v, w=None):
         if w is None:
-            self.graph[u].append(v)
+            self.adj[u].append(v)
         else:
-            self.graph[u].append((v, w))
-        _ = self.graph[v]
+            self.adj[u].append((v, w))
+        _ = self.adj[v]
 
     def ssp_unweighted(self, s):
-        num_v = len(self.graph)
+        num_v = len(self.adj)
         dist = [None] * num_v
         parent = [None] * num_v
         dist[s] = 0
@@ -100,7 +100,7 @@ class Graph:
 
         while len(q) > 0:
             v = q.pop(0)
-            for u in self.graph[v]:
+            for u in self.adj[v]:
                 if dist[u] is None:
                     dist[u] = dist[v] + 1
                     parent[u] = v
@@ -108,7 +108,7 @@ class Graph:
         return dist, parent
 
     def dijkstra(self, s):
-        num_v = len(self.graph)
+        num_v = len(self.adj)
         dist = [INF] * num_v
         parent = [None] * num_v
         dist[s] = 0
@@ -119,7 +119,7 @@ class Graph:
 
         while not q.isempty():
             v = q.extract_min()[0]
-            for u, u_w in self.graph[v]:
+            for u, u_w in self.adj[v]:
                 if not q.contains(u):
                     continue
                 new_d = dist[v] + u_w
@@ -129,6 +129,35 @@ class Graph:
                     q.decrease_key(u, new_d)
 
         return dist, parent
+
+    def bellman_ford(self, s):
+        num_v = len(self.adj)
+        dist = [INF] * num_v
+        parent = [None] * num_v
+
+        dist[s] = 0
+        q = [s]
+        while len(q) > 0:
+            v = q.pop(0)
+            for u, u_w in self.adj[v]:
+                new_d = dist[v] + u_w
+                if new_d < dist[u]:
+                    dist[u] = new_d
+                    parent[u] = v
+
+                q.append(u)
+
+        # detecting -ve cycle in the graph
+        is_valid = True
+        for v in self.adj:
+            for u, u_w in self.adj[v]:
+                if dist[u] > dist[v] + u_w:
+                    is_valid = False
+                    break
+            if not is_valid:
+                break
+
+        return dist, parent, is_valid
 
 if __name__ == '__main__':
     g = Graph()
@@ -154,3 +183,4 @@ if __name__ == '__main__':
     g.addedge(3, 4, 4)
 
     pp(g.dijkstra(0))
+    pp(g.bellman_ford(0))
