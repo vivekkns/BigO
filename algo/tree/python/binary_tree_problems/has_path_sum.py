f90ed4d9e0fa89425cf33518b0a5f4a162e909ff
Vivek Kumar <vivekkumar1987@gmail.com>
2018-06-28 22:25:53 +0530

    added is_leaf def

--- algo/tree/python/binary_tree_problems/has_path_sum.py
+++ algo/tree/python/binary_tree_problems/has_path_sum.py
@@ -5,16 +5,18 @@ from bin_tree_utils import get_bintree, print_tree
 #
 
 
+def is_leaf(root):
+    return root.left is None and root.right is None
+
+
 def has_path_sum(root, rsum):
     if root is None:
         return False
 
-    is_leaf = root.left is None and \
-              root.right is None
-
     rsum -= root.data
-    # reached leaf and rsum is
-    if is_leaf and rsum == 0:
+
+    # reached leaf and rsum is 0
+    if is_leaf(root) and rsum == 0:
         return True
     else:
         return has_path_sum(root.left, rsum) or \
@@ -25,4 +27,5 @@ if __name__ == '__main__':
     btree = get_bintree()
 
     print('has_path_sum(22)? = ', has_path_sum(btree, 22))
+    print('has_path_sum(-45)? = ', has_path_sum(btree, -45))
     print('has_path_sum(11)? = ', has_path_sum(btree, 11))
