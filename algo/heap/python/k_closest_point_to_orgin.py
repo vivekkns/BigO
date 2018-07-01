2be6361d4a87cc70a18176cdff3d77d4582d31cd
Vivek Kumar <vivekkumar1987@gmail.com>
2018-07-01 11:57:43 +0530

    initial commit-closest points to origin

---
+++ algo/heap/python/k_closest_point_to_orgin.py
@@ -0,0 +1,54 @@
+from __future__ import print_function
+from maxheap import maxheap
+
+# Tuples and lists are compared lexicographically using comparison of corresponding elements.
+# This means that to compare equal, each element must compare equal and
+# the two sequences must be of the same type and have the same length.
+
+
+def dist2(p):
+    return p[0]*p[0] + p[1]*p[1]
+
+
+def get_dist(P):
+    d = []
+    for p in P:
+        d.append((dist2(p), p))
+    return d
+
+
+def k_closest(P, k):
+    n = len(P)
+    d = get_dist(P)
+
+    h = maxheap()
+    h.build(d, k)
+
+    for i in range(k, n):
+        if h.getMax() > d[i]:
+            h.decrease_max(d[i])
+
+    return [p[1] for p in h.A[:k]]
+
+
+def k_closest_space_efficient(P, k):
+    n = len(P)
+
+    # build maxheap only for first k elements, S:O(k)
+    d = []
+    for i in range(k):
+        d.append((dist2(P[i]), P[i]))
+
+    h = maxheap()
+    h.build(d, k)
+    for i in range(k, n):
+        dp = (dist2(P[i]), P[i])
+        if h.getMax() > dp:
+            h.decrease_max(dp)
+
+    return [p[1] for p in h]
+
+if __name__ == '__main__':
+    P = [(1, 1), (-4, 7), (0, 2), (12, 15), (-1, -2), (-1, -3), (10, 2), (6, 7)]
+    print (k_closest(P, 4))
+    print (k_closest_space_efficient(P, 4))
---
+++ algo/heap/python/maxheap.py
@@ -0,0 +1,46 @@
+class maxheap:
+    def __init__(self, msize=0):
+        self.msize = msize
+        self.A = [0] * msize
+        # size index starts from 1 NOT 0
+        self.size = 0
+
+    def _swap(self, i, j):
+        self.A[i], self.A[j] = self.A[j], self.A[i]
+
+    def build(self, A, size):
+        self.A = A
+        self.size = size
+        for i in range(self.size/2, -1, -1):
+            self.sink(i)
+
+    def swim(self, i):
+        while i > 0 and self.A[i] > self.A[(i-1)/2]:
+            self._swap(i, (i-1)/2)
+            i = (i-1)/2
+
+    def sink(self, i):
+        m = i
+
+        l = 2*i + 1
+        r = 2*i + 2
+
+        if l < self.size and self.A[l] > self.A[m]:
+            m = l
+        if r < self.size and self.A[r] > self.A[m]:
+            m = r
+
+        if m != i:
+            self._swap(i, m)
+            self.sink(m)
+
+    def getMax(self):
+        return self.A[0]
+
+    def decrease_max(self, data):
+        self.A[0] = data
+        self.sink(0)
+
+    def __iter__(self):
+        for a in self.A:
+            yield a
