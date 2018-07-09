4852015222cc7de1f8e428df4cd2eb14bc97d3cd
Vivek Kumar <vivekkumar1987@gmail.com>
2018-07-09 17:52:20 +0530

    initial commit

---
+++ golang/13_web_app.go
@@ -0,0 +1,34 @@
+package main
+
+import ("fmt"
+	"encoding/xml"
+	"net/http"
+	"io/ioutil"
+)
+
+type SitemapIndex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>Keywords"`
+	Locations []string `xml:"url>loc"`
+
+
+}
+func main() {
+	var s SitemapIndex
+	var n News
+
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &s)
+
+	for _, Location := range s.Locations{
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+		fmt.Println(n)
+	}
+}
