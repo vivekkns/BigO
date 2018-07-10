17355ca1e523820dc856b66ed6bac54153f9147a
Vivek Kumar <vivekkumar1987@gmail.com>
2018-07-10 17:39:18 +0530

    intro tp goroutine

---
+++ golang/18_goroutines.go
@@ -0,0 +1,18 @@
+package main
+
+import ("time"
+	"fmt"
+)
+
+func say(s string){
+	for i := 0; i < 3; i++ {
+		fmt.Println(s)
+		time.Sleep(time.Millisecond * 100)
+	}
+}
+
+func main() {
+	go say("Hey")
+	go say("There")
+	time.Sleep(time.Second)
+}
